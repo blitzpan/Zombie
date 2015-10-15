3e9adf64b5cb2a71e9197f5bc1eaa56f2600912d
pyk <panyk@hdsxtech.com>
2015-10-15 14:52:01 +0800

    test

---
+++ src/AZombie.java
@@ -0,0 +1,14 @@
+
+public abstract class AZombie {
+	int x;
+	int y;
+	int xs;
+	int dir = 1;//1ÍùÓÒ£¬-1Íù×ó
+	int life = 1000;
+	int hurt = 5;
+	String img = "";
+	int type = 0;
+	public void move(){
+		
+	}
+}
